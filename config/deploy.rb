0b009a2e77183258796aacd5ecaed910af07a2eb
Dex Go <dexterfgo@gmail.com>
2017-08-04 19:39:04 +1000

    Remove linked bin directory in release.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,10 @@ set :repo_url, "git@github.com:dexterfgo/foosball.git"
 set :branch, :master
 set :deploy_to, '/home/deploy/foosball'
 set :pty, true
+set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.4.1' # Edit this if you are using MRI Ruby
